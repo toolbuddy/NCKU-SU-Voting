b40208672aaa2fbe8406c7ad0261b82c68f091b0
Jack Huang <f26401004@gmail.com>
2018-09-27 16:29:10 +0800

    Revert "電腦版觀看圖片會置中了"

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,5 @@
 <template>
   <div>
-
-    <div  class="container">
-      <div  class="_container">
-      <h3>成大學生會</h3>
-      </div>
-      <img class="menuphoto" src="~/assets/img/options.png"> 
-    </div>
-    <!--
-    temporarily hide the FB login button
     <div  class="container">
       <bighead :isLogin="isLogin" :profile="profile"></bighead>
       <div v-if="!isLogin">
@@ -18,18 +9,14 @@
           <button type="button" v-on:click="logout"> Logout</button>
       </div>
     </div>
-    -->
-
     <img class="homephoto" src="~/assets/img/home.png">
 
     <div class="hometitle">
       <div class="_hometitle">
-      <h1>WHY辯論會？</h1>
-      <h2>一些副標文字，<br>說明這則新聞的內容。</h2>
+      WHY辯論會？<br>
+      一些副標文字，說明這則新聞的內容。
       </div>
     </div>
-
-
   </div>
 </template>
 
@@ -118,131 +105,25 @@ export default {
 </script>
 
 <style scoped>
-
-h1{
-font-family: ‘segoe ui’, sans-serif;
-font-size: 26px;
-font-weight: 100px;
-color: white;
-}
-h2{
-font-family: ‘segoe ui’, sans-serif;
-font-size: 18px;
-font-weight: 100px;
-color: white;
-}
-h3{
-font-family: ‘segoe ui’, sans-serif;
-font-size: 16px;
-font-weight: 100px;
-color: #A2A2A2;
-}
-
-@media screen and  (orientation:landscape) 
+@media screen and (min-width: 600 px) and (max-width: 2560 px)
 {
-      
-      img.homephoto 
-      {
-      position: absolute;
-      left: 35%;
-      top: 7%;
-      height: 70%;
-      width: 70%;
-      display: block;
-      margin: auto;
-      }
-      .container
-      {
-        position: absolute;
-        top: 0%;
-        height:20%;
-        width: 100%;
-        background-color:rgba(223, 223, 223, 0.151);
-      }
-      .hometitle 
-      {
-       position: absolute;
-       top: 50%;
-       left: 0%;
-       width: 100%;
-       height:30vh;
-       margin: auto;
-       background-color:rgba(81,81,81,0.48);
-       border:0px;
-       z-index: 0;
-      }
-
-      ._hometitle {
-          position: absolute;
-          top: 5%;
-          left: 10%;
-          height: 10vh;
-          text-align: left;
-      }
-
+  img.homephoto 
+  {
+    position: fixed;
+    top: 10%;
+    height: 70%;
+    width: auto;
+    display: block;
+    margin: auto;
+  }
 }
 
-@media screen and(orientation:landscape) and(max-width: width 1024px)
-
-{
-      img.homephoto 
-      {
-      position: absolute;
-      left: 35%;
-      top: 5%;
-      height: 300px;
-      width: 300px;
-      display: block;
-      margin: auto;
-      }
-      .container
-      {
-        position: absolute;
-        top: 0%;
-        height:40px;
-        width: 100%;
-        background-color:rgba(223, 223, 223, 0.151);
-      }
-      .hometitle 
-      {
-       position: absolute;
-       top: 50%;
-       left: 0%;
-       width: 100%;
-       height:30vh;
-       margin: auto;
-       background-color:rgba(81,81,81,0.48);
-       border:0px;
-       z-index: 0;
-      }
-
-      ._hometitle {
-          position: absolute;
-          top: 5%;
-          left: 10%;
-          height: 10vh;
-          text-align: left;
-      }
-
-     }
-     /* orientation: portrait   直式*/
-     /* orientation: landscape  橫式*/
-
-img.homephoto 
+  img.homephoto 
   {
     position: fixed;
     top: 20%;
     height: 56%;
     width: auto;
-    display: block;
-    margin: auto;
-  }
-
-  img.menuphoto 
-  {
-    position: fixed;
-    top: 10px;
-    right: 3%;
     display: block;
     margin: auto;
   }
@@ -253,7 +134,7 @@ img.homephoto
     top: 64.3%;
     left: 0%;
     width: 100%;
-    height:23vh;
+    height:20vh;
     margin: auto;
     background-color:rgba(81,81,81,0.48);
     border:0px;
@@ -262,7 +143,7 @@ img.homephoto
 
 ._hometitle {
     position: absolute;
-    top: 10%;
+    top: 15%;
     left: 10%;
     height: 10vh;
     text-align: left;
@@ -276,14 +157,6 @@ img.homephoto
   height:6.1%;
   width: 100%;
   background-color:rgba(223, 223, 223, 0.151);
-}
-._container
-{
-  position: fixed;
-  top: 0px;
-  left: 3%;
-  display: block;
-  margin: auto;
 }
 
 
